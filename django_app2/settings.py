c48277662265c2393d1a790f23177d51d676f235
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-15 22:17:34 +0900

    update

--- django_app2/settings.py
+++ django_app2/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import dj_database_url
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
