467186229ff481ba13ad5cc3728be8e8f9232ba3
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-16 20:06:21 +0900

    update3

--- django_app2/settings.py
+++ django_app2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import dj_database_url
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,6 +126,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#以下怪しい部分
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
