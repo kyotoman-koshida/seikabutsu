02c510b00cb7bb211239bfa5da3a2a8dd4fbf9f9
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-24 00:13:06 +0900

    'commit'

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app2.settings', 'django_app2.local_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
