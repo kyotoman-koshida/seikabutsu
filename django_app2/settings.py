649fdf911f55b35bec28ea9a424065aab99191c7
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-24 14:00:36 +0900

    'commit'

--- django_app2/settings.py
+++ django_app2/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com',  'kyotomanapp.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com',  'kyotoman-app.herokuapp.com']
 
 
 # Application definition
