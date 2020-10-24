4b26dbe74e6243163f04bfffa281f68f700cb34a
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-24 17:04:18 +0900

    'commit'

--- django_app2/settings.py
+++ django_app2/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com',  'kyotoman-app.hero
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
