e72604980da0ebc4010d7513ce2c78c1fe9a8a1c
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-31 15:27:44 +0900

    'commit'

--- django_app2/settings.py
+++ django_app2/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
     
 ]
 
