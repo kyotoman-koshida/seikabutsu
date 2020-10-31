a1392b093e893592286edbb9dff045d7079479b8
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-31 15:17:04 +0900

    'commit'

--- django_app2/urls.py
+++ django_app2/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from sns.views import index
 from django.conf.urls import url
 
+app_name = 'social_django'
+
 urlpatterns = [
     #/login/twitter/, /complete/twitter/, /disconnect/twitter/に対応
     path('', include('social_django.urls')),
