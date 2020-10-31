73c497ed82fa23bb2b32c10713b3629829c8f1df
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-10-31 14:48:08 +0900

    'commit'

--- django_app2/settings.py
+++ django_app2/settings.py
@@ -176,6 +176,10 @@ except ImportError:
 
 if not DEBUG:    
    SECRET_KEY = os.environ['SECRET_KEY']
+   SOCIAL_AUTH_TWITTER_KEY = os.environ['SOCIAL_AUTH_TWITTER_KEY']
+   SOCIAL_AUTH_TWITTER_SECRET = os.environ['SOCIAL_AUTH_TWITTER_SECRET']
+   AUTHENTICATION_TOKEN = os.environ['AUTHENTICATION_TOKEN']
+   AUTHENTICATION_SECRET = os.environ['AUTHENTICATION_SECRET']
    django_heroku.settings(locals())
    """
    SOCIAL_AUTH_TWITTER_KEY = os.environ['SOCIAL_AUTH_TWITTER_KEY']
