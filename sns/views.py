6c91fb3bcc09a1a0851d7679ef630932fb5aea79
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-11-02 01:16:45 +0900

    'commit'

--- sns/views.py
+++ sns/views.py
@@ -166,7 +166,7 @@ def groups(request):
 def add(request):
     # 追加するUserを取得
     add_name = request.GET['name']
-    add_user = User.objects.filter(username=add_name).first()
+    add_user = User.objects.filter(email=add_name).first()
     # Userが本人だった場合の処理
     if add_user == request.user:
         messages.info(request, "自分自身をFriendに追加することは\
