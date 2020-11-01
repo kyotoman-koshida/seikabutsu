174478ab8ea54c23ae3ce7678a9bf5d39751a1fa
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-11-01 11:46:44 +0900

    'commit'

--- django_app2/urls.py
+++ django_app2/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     #/login/twitter/, /complete/twitter/, /disconnect/twitter/に対応
-    url('', include('social_django.urls', namespace='social')),
+    url('', include('social_django.urls')),
     url('admin/', admin.site.urls),    
     url('sns/',include('sns.urls', namespace='sns')),
     url('', index, name='top'),
