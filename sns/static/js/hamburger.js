d00345b1d499a01cb6e43a21a00073dcadbe34ea
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2020-11-09 09:27:02 +0900

    'ユーザー表示の修正'

--- sns/static/js/hamburger.js
+++ sns/static/js/hamburger.js
@@ -1,15 +1,5 @@
-  const open = document.querySelector("#open");
-  const close = document.querySelector("#close");
-  const g_menu = document.querySelector("#g_menu");
-  
-  //メニューを出す
-  open.addEventListener('click', function() {
-      g_menu.classList.add('inside');
-      g_menu.classList.remove('outside');
-   }, false);
-  
-  //メニューを隠す
-  close.addEventListener('click', function() {
-      g_menu.classList.remove('inside');
-      g_menu.classList.add('outside');
-   }, false);  
+$(function(){
+  $('.hamburger-menu').on('click', function() {
+    $(this).toggleClass('hamburger-menu-active')
+  })
+})
