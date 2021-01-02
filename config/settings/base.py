80079b207ecf50d5e02266546b577966e74af98e
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2021-01-02 21:22:15 +0900

    'media'

--- config/settings/base.py
+++ config/settings/base.py
@@ -113,7 +113,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),  # プロジェクト直下のstaticディレクトリを指定
+    os.path.join(BASE_DIR, '/static','/media'),  # プロジェクト直下のstaticディレクトリを指定
     #'static/',
 )
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
