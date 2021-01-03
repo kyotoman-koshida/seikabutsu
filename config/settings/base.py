498ebf3fb57f305b0a788d660bffde30e688a20d
kyotoman <71860531+kyotoman-koshida@users.noreply.github.com>
2021-01-03 12:10:14 +0900

    'media'

--- config/settings/base.py
+++ config/settings/base.py
@@ -110,7 +110,7 @@ AUTH_USER_MODEL = 'account.User'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '/static','/media'),  # プロジェクト直下のstaticディレクトリを指定
